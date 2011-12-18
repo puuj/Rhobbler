d6c9fd9dba0bfca2ef28737c9c524f12fbf23c8d
Erik Peterson <erik@subwindow.com>
2011-12-17 21:45:41 -0500

    Attempting to get unicorn working correctly

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ worker_processes 2
 preload_app false
 timeout 30
 listen "/tmp/rhobbler.sock", :backlog => 64
-pid "/sites/rhobbler/tmp/pids/unicorn.pid"
-stderr_path "/sites/rhobbler/log/unicorn.log"
-stdout_path "/sites/rhobbler/log/unicorn.log"
+pid "/home/rhobbler2/shared/unicorn.pid"
+stderr_path "/sites/rhobbler/shared/log/unicorn.log"
+stdout_path "/sites/rhobbler/shared/log/unicorn.log"
