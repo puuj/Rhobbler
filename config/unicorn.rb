7341fc327392fd337433c0d13cef98f43ee659d8
Erik Peterson <erik@subwindow.com>
2011-12-18 13:33:13 -0500

    Don't need unicorn config in source control

--- config/unicorn.rb
+++
@@ -1,7 +0,0 @@
-worker_processes 2
-preload_app false
-timeout 30
-listen "/home/rhobbler2/shared/sockets/rhobbler2.sock", :backlog => 64
-pid "/home/rhobbler2/shared/unicorn.pid"
-stderr_path "/home/rhobbler2/shared/log/unicorn.log"
-stdout_path "/home/rhobbler2/shared/log/unicorn.log"
