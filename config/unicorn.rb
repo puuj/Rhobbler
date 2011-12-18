182e49fa3ec525491789c1dbbdb4a6cdccac0e6e
Erik Peterson <erik@subwindow.com>
2011-12-18 09:46:11 -0500

    More deploy stuff

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 worker_processes 2
 preload_app false
 timeout 30
-listen "/tmp/rhobbler.sock", :backlog => 64
+listen "/home/rhobbler2/shared/sockets/rhobbler2.sock", :backlog => 64
 pid "/home/rhobbler2/shared/unicorn.pid"
 stderr_path "/sites/rhobbler/shared/log/unicorn.log"
 stdout_path "/sites/rhobbler/shared/log/unicorn.log"
