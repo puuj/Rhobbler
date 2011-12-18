4500020ecf19dc66192ad4179072b278aa0eb552
Erik Peterson <erik@subwindow.com>
2011-12-18 09:48:52 -0500

    Fix logfile paths

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,5 +3,5 @@ preload_app false
 timeout 30
 listen "/home/rhobbler2/shared/sockets/rhobbler2.sock", :backlog => 64
 pid "/home/rhobbler2/shared/unicorn.pid"
-stderr_path "/sites/rhobbler/shared/log/unicorn.log"
-stdout_path "/sites/rhobbler/shared/log/unicorn.log"
+stderr_path "/home/rhobblerw/shared/log/unicorn.log"
+stdout_path "/home/rhobbler2/shared/log/unicorn.log"
